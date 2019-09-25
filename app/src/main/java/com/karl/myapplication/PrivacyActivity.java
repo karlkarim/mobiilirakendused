c32e3b2d53f2053ad1036baeee6966ff5c4c892e
opilane <opilane@HP-CZC7398HF3>
2019-09-25 16:31:07 +0300

    jjjjd

--- app/src/main/java/com/karl/myapplication/PrivacyActivity.java
+++ app/src/main/java/com/karl/myapplication/PrivacyActivity.java
@@ -2,13 +2,54 @@ package com.karl.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.Toast;
 
 public class PrivacyActivity extends AppCompatActivity {
+
+    //declaring my variables
+    private CheckBox agreed;
+    private CountDownTimer timer;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_privacy);
+
+        //bonding with the graphical side'
+        agreed = findViewById(R.id.checkBox);
+        timer = new CountDownTimer(8000,1000) {
+            @Override
+            public void onTick(long l) {
+
+            }
+
+            @Override
+            public void onFinish() {
+                Toast.makeText(PrivacyActivity.this,
+                        getResources().getString(R.string.arg), Toast.LENGTH_LONG).show();
+                this.start();//this will replay the countdown again
+
+            }
+        }.start();
+
+        agreed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                boolean checked = ((CheckBox)view).isChecked();
+                if(checked){
+                    //this will start a new activity like take user to next page
+                    Intent start = new Intent(getApplicationContext(),MainActivity.class);
+                    startActivity(start);
+                    //we want the timer to stop giving us toast message
+                    timer.cancel();
+                }
+            }
+        });
     }
 }
