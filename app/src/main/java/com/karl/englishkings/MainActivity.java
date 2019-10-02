eee7bd15a92897baffbf7274dec830e1d8f8764e
opilane <karl.karimoisa@tmd.ee>
2019-10-02 15:50:37 +0300

    teee

--- app/src/main/java/com/karl/englishkings/MainActivity.java
+++ app/src/main/java/com/karl/englishkings/MainActivity.java
@@ -3,6 +3,8 @@ package com.karl.englishkings;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -24,9 +26,23 @@ public class MainActivity extends AppCompatActivity {
         to = findViewById(R.id.txtTo);
         kingList = findViewById(R.id.lstKings);
 
-        kingList.setAdapter(new ArrayAdapter<>(this,android.R.layout.simple_list_item_1, kings));
+        kingList.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, kings));
 
-        
+        kingList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, final int position, long l) {
+                update(position);
+            }
+        });
+
+    }
+        private void update(int position){
+            int a = kings.get(position).getFrom();
+            int b = kings.get(position).getTo();
+            from.setText(a == 0 ? "" : "" + a);
+            to.setText(b==9999 ? "" :"" + b);
+
+
 
     }
 }
